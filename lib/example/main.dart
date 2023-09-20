38038ac00c53ba8f058dc52a8f11b5bcc2a59092
whitestorm0316 <whitestorm0316@gmail.com>
2023-09-20 23:36:03 +0800

    add pick file and read

--- lib/example/main.dart
+++ lib/example/main.dart
@@ -1,112 +1,76 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:open_file/open_file.dart';
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:speech_to_text/speech_to_text.dart';
+import 'dart:io';
+import 'package:spreadsheet_decoder/spreadsheet_decoder.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  void _pickFile() async {
+    // opens storage to pick files and the picked file or files
+    // are assigned into result and if no file is chosen result is null.
+    // you can also toggle "allowMultiple" true or false depending on your need
+    final result = await FilePicker.platform.pickFiles(allowMultiple: true);
+
+    // if no file is picked
+    if (result == null) return;
+
+    // we get the file from result object
+    final file = result.files.first;
+
+    print(file.path);
+    _parseFile(file);
+  }
+
+  void _parseFile(PlatformFile file) {
+    var bytes = File.fromUri(Uri(path: file.path)).readAsBytesSync();
+    var decoder = SpreadsheetDecoder.decodeBytes(bytes);
+    stderr.writeln((decoder.tables).keys);
+    debugPrint('debug print');
+    debugPrint((decoder.tables).keys.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  SpeechToText _speechToText = SpeechToText();
-  bool _speechEnabled = false;
-  String _lastWords = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _initSpeech();
-  }
-
-  /// This has to happen only once per app
-  void _initSpeech() async {
-    _speechEnabled = await _speechToText.initialize();
-    setState(() {});
-  }
-
-  /// Each time to start a speech recognition session
-  void _startListening() async {
-    await _speechToText.listen(onResult: _onSpeechResult);
-    setState(() {});
-  }
-
-  /// Manually stop the active speech recognition session
-  /// Note that there are also timeouts that each platform enforces
-  /// and the SpeechToText plugin supports setting timeouts on the
-  /// listen method.
-  void _stopListening() async {
-    await _speechToText.stop();
-    setState(() {});
-  }
-
-  /// This is the callback that the SpeechToText plugin calls when
-  /// the platform returns recognized words.
-  void _onSpeechResult(SpeechRecognitionResult result) {
-    setState(() {
-      _lastWords = result.recognizedWords;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Speech Demo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(16),
-              child: Text(
-                'Recognized words:',
-                style: TextStyle(fontSize: 20.0),
-              ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: Center(
+          child: MaterialButton(
+            onPressed: () {
+              _pickFile();
+            },
+            child: Text(
+              'Pick and open file',
+              style: TextStyle(color: Colors.white),
             ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  // If listening is active show the recognized words
-                  _speechToText.isListening
-                      ? '$_lastWords'
-                      // If listening isn't active but could be tell the user
-                      // how to start it, otherwise indicate that speech
-                      // recognition is not yet ready or not supported on
-                      // the target device
-                      : _speechEnabled
-                          ? 'Tap the microphone to start listening...'
-                          : 'Speech not available',
-                ),
-              ),
-            ),
-          ],
+            color: Colors.green,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:
-            // If not yet listening for speech start, otherwise stop
-            _speechToText.isNotListening ? _startListening : _stopListening,
-        tooltip: 'Listen',
-        child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
-      ),
     );
   }
+}
+
+void _pickFile() async {
+  // opens storage to pick files and the picked file or files
+  // are assigned into result and if no file is chosen result is null.
+  // you can also toggle "allowMultiple" true or false depending on your need
+  final result = await FilePicker.platform.pickFiles(allowMultiple: true);
+
+  // if no file is picked
+  if (result == null) return;
+
+  // we get the file from result object
+  final file = result.files.first;
+
+  _openFile(file);
+}
+
+void _openFile(PlatformFile file) {
+  OpenFile.open(file.path);
 }
